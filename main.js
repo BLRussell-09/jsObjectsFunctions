86ac1857e694a7c362e36388e6066ecf6bc7f117
Barry Russell <blrussell09@gmail.com>
2018-03-03 10:10:15 -0600

    Added superPets

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("East-side Boyz")
+var favAnimals = {
+  name: "Barry", animal: "Ghost Dog"
+}
+
+var superPets = [
+  {
+    instructorName: "Barry",
+    petName: "Kobe",
+    animalType: "Ghost Dog",
+    isFavorite: true,
+  },
+  {
+    instructorName: "Barry",
+    petName: "Rikku",
+    animalType: "Cat",
+    isFavorite: false,
+  },
+  {
+    instructorName: "Barry",
+    petName: "Ginger",
+    animalType: "Live Dog",
+    isFavorite: false,
+  }
+];
+
+console.log(superPets);
+var divElement = document.getElementById("petName")
+var output = '';
+for(i=0; i < superPets.length; i++){
+  divElement.innerHTML += "<h4>" + superPets[i].petName + "</h4>"
+  output += superPets[i].petName;
+}
+
+
